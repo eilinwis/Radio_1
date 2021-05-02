a5f45ebdf6bc459fd320c36a653b2dac818194ed
Roman Kudryavtsev <TTe4kiH98@gmail.com>
2021-05-02 21:42:18 +0300

    Lombokconfig added

--- src/main/java/ru/netology/Radio.java
+++ src/main/java/ru/netology/Radio.java
@@ -1,21 +1,18 @@
 package ru.netology;
 
-import lombok.AllArgsConstructor;
+
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
-
 public class Radio {
-    String name;
     public int maxStation = 10;
     public int minStation = 0;
     public int currentStation;
     public int maxVolume = 100;
     public int minVolume = 0;
     public int currentVolume;
-    public boolean on;
 
     public Radio(int maxStation) {
         this.maxStation = maxStation;
